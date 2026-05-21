c3296156144efbaf92fa38890020c3aa973f3a6f
khidmatcom <khidmatcom.dz@gmail.com>
2026-05-21 21:25:13 +0100

    Update khidmatcom.config.example.js

--- khidmatcom.config.example.js
+++ khidmatcom.config.example.js
@@ -1,4 +1,4 @@
 const KhidmatcomConfig = {
   GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw5VSR1dLAxuLNQEPg6V5azYu3plkJwZSmvyUeoKOQhKF58ingJPzCXAC8JrCfS2eUYsQ/exec',
-  SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/YOUR_SPREADSHEET_ID/edit'
+  SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/1oxzbXW3BhJPI___8DUpCbFHF5cAg051VlZi7i_oEDbs/edit''
 };
