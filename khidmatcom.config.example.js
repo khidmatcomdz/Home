9b64155ef13712e0aac7f0373bc2ce82bdd15598
khidmatcom <khidmatcom.dz@gmail.com>
2026-05-21 20:33:45 +0100

    Delete khidmatcom.config.example.js

--- khidmatcom.config.example.js
+++
@@ -1,4 +0,0 @@
-const KhidmatcomConfig = {
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
-  SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/YOUR_SPREADSHEET_ID/edit'
-};
