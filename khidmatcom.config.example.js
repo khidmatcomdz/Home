b48ed965c2ab753c6ad2d406f434c099cd712eeb
khidmatcom <khidmatcom.dz@gmail.com>
2026-05-21 20:50:54 +0100

    Update khidmatcom.config.example.js

--- khidmatcom.config.example.js
+++ khidmatcom.config.example.js
@@ -1,4 +1,4 @@
 const KhidmatcomConfig = {
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyhC8r9ba9UsCNxT-7zcZq_5js4UnD1mZPNmsealMJr5ClsO26jCJ2QgjSMMg_yGsGh_w/exec',
   SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/YOUR_SPREADSHEET_ID/edit'
 };
